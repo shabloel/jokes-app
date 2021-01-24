0b11aca7369e5652371b41d62dc8fc5369cdbf80
chris <cgirecruitment19@gmail.com>
2021-01-24 16:42:40 +0100

    new code

--- src/main/java/com/jokes/jokeapp/service/JokeServiceImpl.java
+++ src/main/java/com/jokes/jokeapp/service/JokeServiceImpl.java
@@ -2,6 +2,7 @@ package com.jokes.jokeapp.service;
 
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,9 +10,9 @@ public class JokeServiceImpl implements JokeService{
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    @Autowired
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
